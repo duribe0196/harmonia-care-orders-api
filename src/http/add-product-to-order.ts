e8d64a54d322b8c06d80b68d212536c2640f3660
duribegl <110846568+duribegl@users.noreply.github.com>
2024-05-29 13:56:38 -0500

    add-product-to-order.ts add info logs

--- src/http/add-product-to-order.ts
+++ src/http/add-product-to-order.ts
@@ -1,7 +1,7 @@
-import {IOrderProduct} from "../db/models/order";
+import { IOrderProduct } from "../db/models/order";
 import Product from "../db/models/product";
 import ShoppingCart from "./ShoppingCart";
-import {MongooseError} from "mongoose";
+import { MongooseError } from "mongoose";
 import User from "../db/models/user";
 
 interface IAddProductToOrderArgs {
@@ -12,42 +12,65 @@ interface IAddProductToOrderArgs {
 
 export default async function addProductsToOrder(
   args: IAddProductToOrderArgs,
-): Promise<{ body: string; statusCode: number; sessionId?: string; }> {
-  const {
-    product,
-    userSub,
-    sessionId,
-  } = args;
+): Promise<{ body: string; statusCode: number; sessionId?: string }> {
+  const { product, userSub, sessionId } = args;
   try {
-    console.log(`Add product to order - init new order with product ${product.productId.toString()}`);
-    const user = await User.findOne({sub: userSub})
-    const productFound = await Product.findById(product.productId)
+    let user;
+    console.log(
+      `Add product to order - init new order with product ${product.productId.toString()}`,
+    );
+    if (userSub) {
+      console.log(
+        `Add product to order - looking for user with sub ${userSub}`,
+      );
+      user = await User.findOne({ sub: userSub });
+    }
+    console.log(
+      `Add product to order - looking for product ${product.productId.toString()}`,
+    );
+    const productFound = await Product.findById(product.productId);
     if (!productFound) {
+      console.error(
+        `Add product to order - No product found ${product.productId.toString()}`,
+      );
       return {
         statusCode: 400,
         body: JSON.stringify({ message: "Product not found" }),
       };
     }
+    const requestorMessage = user?.id
+      ? "user id: " + user?.id
+      : "session id: " + sessionId;
+    console.log(`Add product to order - getting order for ${requestorMessage}`);
     const newShoppingCart = new ShoppingCart(user?.id || null, sessionId);
-    const updatedCart = await newShoppingCart.addProduct(product.productId, product.quantity)
+
+    console.log(
+      `Add product to order - adding product for order - for ${requestorMessage}`,
+    );
+    const updatedCart = await newShoppingCart.addProduct(
+      product.productId,
+      product.quantity,
+    );
+    console.log(
+      `Add product to order - product added for order - for ${requestorMessage}`,
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(updatedCart),
-      sessionId: sessionId
+      sessionId: sessionId,
     };
   } catch (e: unknown) {
-    if(e instanceof MongooseError){
-        console.error(e)
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ message: "Error updating order" }),
-        };
+    if (e instanceof MongooseError) {
+      console.error(e);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: "Error updating order" }),
+      };
     }
-    console.error(e)
+    console.error(e);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: "Something went wrong" }),
-    }
-
+    };
   }
 }
